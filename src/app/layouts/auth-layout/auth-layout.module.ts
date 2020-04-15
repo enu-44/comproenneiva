6481f5e9d9ab6bbe330ee4977f257053649021c1
German Aviles <desarrollador.junior2@interedes.com.co>
2020-04-15 17:40:28 -0500

    material agregado

--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-import { AuthLayoutRoutes } from "./auth-layout.routing";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import { AuthLayoutRoutes } from "./auth-layout.routing";
 import { LoginComponent } from "../../pages/examples/login/login.component";
 import { ConfirmacionComponent } from "../../pages/examples/confirmacion/confirmacion.component";
 import { PricingComponent } from "../../pages/examples/pricing/pricing.component";
 import { LockComponent } from "../../pages/examples/lock/lock.component";
 import { RegisterComponent } from "../../pages/examples/register/register.component";
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
+    MatExpansionModule,
     CommonModule,
     RouterModule.forChild(AuthLayoutRoutes),
     FormsModule
--- src/app/pages/negocio/negocio.module.ts
+++ src/app/pages/negocio/negocio.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NegocioFormComponent } from './negocio-form/negocio-form.component';
-
 import { RouterModule } from "@angular/router";
 import { NegocioRoutes } from "./negocio.routing";
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [NegocioFormComponent],
   imports: [
     CommonModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatTabsModule,
     RouterModule.forChild(NegocioRoutes)
   ]
 })
