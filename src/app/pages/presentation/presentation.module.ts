4fa3d75228a4030394f23c938f78ba572feed9ff
julian camilo bonilla leyva <julian.bonilla@interedes.com.co>
2020-04-15 14:30:20 -0500

    Ajustes form y mapa

--- src/app/pages/presentation/presentation.module.ts
+++ src/app/pages/presentation/presentation.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from "@angular/common";
 import { TooltipModule } from "ngx-bootstrap/tooltip";
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
-
+import { AgmCoreModule } from '@agm/core';
 import { RouterModule } from "@angular/router";
 import { PresentationComponent } from "./presentation.component";
 
 @NgModule({
   declarations: [PresentationComponent],
-  imports: [CommonModule, RouterModule, TooltipModule.forRoot(), BsDropdownModule.forRoot(), CollapseModule.forRoot()]
+  imports: [
+    CommonModule, 
+    RouterModule, 
+    TooltipModule.forRoot(), 
+    BsDropdownModule.forRoot(), 
+    CollapseModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyARIZgEvVm-isf97lDF5HmAwom6N7ftFg8'
+    })
+  ]
 })
 export class PresentationModule {}
