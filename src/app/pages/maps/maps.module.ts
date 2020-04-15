ce9f8e208131b714f5ccd55c017f7dd66c10a42e
Enuar.Munoz <enuar.munoz@interedes.com.co>
2020-04-15 09:20:41 -0500

    integracion mapas agm ok

--- src/app/pages/maps/maps.module.ts
+++ src/app/pages/maps/maps.module.ts
@@ -8,12 +8,17 @@ import { MapsRoutes } from "./maps.routing";
 
 import { GoogleComponent } from "./google/google.component";
 import { VectorComponent } from "./vector/vector.component";
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [GoogleComponent, VectorComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(MapsRoutes),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyARIZgEvVm-isf97lDF5HmAwom6N7ftFg8'
+    }),
     ComponentsModule,
     DxVectorMapModule
   ]
