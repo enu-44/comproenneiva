904380cf2fd1f1eef9809be88d8aad2dc081379f
German Aviles <desarrollador.junior2@interedes.com.co>
2020-04-16 17:08:35 -0500

    horario version 1 lista

--- src/app/pages/negocio/negocio-form/negocio-form.component.ts
+++ src/app/pages/negocio/negocio-form/negocio-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import Selectr from "mobius1-selectr";
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import { MrsComponent } from '../../../components/mrs/mrs.component';
+declare var $: any;
 
 @Component({
   selector: 'app-negocio-form',
@@ -12,27 +14,119 @@ export class NegocioFormComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-
+  dias: any [] = [];
+  time = {hour: 13, minute: 30};
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     var selectr: any = document.getElementById("selectr");
     var options = {};
     var optionsMultiple = { multiple: true };
-    // var selectorDefault = new Selectr(selectr, options);
     var selectrmultiple: any = document.getElementById("selectr-multiple");
 
-    // var selectorMultiple = new Selectr(selectrmultiple, optionsMultiple);
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
-  }
 
-  onSliderChange(selectedValues: number[]) {
-    console.log('VALOR: ', selectedValues);
+    this.dias.push(
+      {
+        nombre: 'lunes',
+        jornadaContinua: false,
+        continua: {
+          jornada: 'jornada continua',
+          activa: false,
+          horaInicio: { hour: 0, minute: 0 },
+          horaFin: { hour: 0, minute: 0 }
+        },
+        jornadas: [
+          {
+            jornada: 'mañana',
+            activa: true,
+            horaInicio: { hour: 8, minute: 0 },
+            horaFin: { hour: 11, minute: 59 }
+          },
+          {
+            jornada: 'tarde',
+            activa: true,
+            horaInicio: { hour: 0, minute: 0 },
+            horaFin: { hour: 0, minute: 0 }
+          },
+          {
+            jornada: 'noche',
+            activa: true,
+            horaInicio: { hour: 19, minute: 0 },
+            horaFin: { hour: 3, minute: 0 }
+          }
+        ]
+      },
+      {
+        nombre: 'martes',
+        jornadaContinua: true,
+        continua: {
+          jornada: 'jornada continua',
+          activa: false,
+          horaInicio: { hour: 6, minute: 0 },
+          horaFin: { hour: 19, minute: 0 }
+        },
+        jornadas: [
+          {
+            jornada: 'mañana',
+            activa: true,
+            horaInicio: { hour: 8, minute: 0 },
+            horaFin: { hour: 11, minute: 59 }
+          },
+          {
+            jornada: 'tarde',
+            activa: true,
+            horaInicio: { hour: 0, minute: 0 },
+            horaFin: { hour: 0, minute: 0 }
+          },
+          {
+            jornada: 'noche',
+            activa: true,
+            horaInicio: { hour: 19, minute: 0 },
+            horaFin: { hour: 3, minute: 0 }
+          }
+        ]
+      },
+      {
+        nombre: 'miercoles',
+        jornadaContinua: false,
+        continua: {
+          jornada: 'jornada continua',
+          activa: false,
+          horaInicio: { hour: 0, minute: 0 },
+          horaFin: { hour: 0, minute: 0 }
+        },
+        jornadas: [
+          {
+            jornada: 'mañana',
+            activa: false,
+            horaInicio: { hour: 0, minute: 0 },
+            horaFin: { hour: 0, minute: 0 }
+          },
+          {
+            jornada: 'tarde',
+            activa: true,
+            horaInicio: { hour: 13, minute: 0 },
+            horaFin: { hour: 16, minute: 0 }
+          },
+          {
+            jornada: 'noche',
+            activa: true,
+            horaInicio: { hour: 19, minute: 0 },
+            horaFin: { hour: 3, minute: 0 }
+          }
+        ]
+      }
+    )
+
   }
+  // onSliderChange(selectedValues: number[]) {
+  //   console.log('VALOR: ', selectedValues);
+  // }
 
 }
--- src/app/pages/negocio/negocio.module.ts
+++ src/app/pages/negocio/negocio.module.ts
@@ -1,25 +1,34 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NegocioFormComponent } from './negocio-form/negocio-form.component';
+import { MrsComponent } from '../../components/mrs/mrs.component';
 import { RouterModule } from "@angular/router";
 import { NegocioRoutes } from "./negocio.routing";
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NpnSliderModule } from "npn-slider";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
-  declarations: [NegocioFormComponent],
+  declarations: [
+    NegocioFormComponent,
+    MrsComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
     MatExpansionModule,
     MatInputModule,
     MatTabsModule,
-    NpnSliderModule,
-    RouterModule.forChild(NegocioRoutes),
-    // NgbModule
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    // NpnSliderModule,
+    NgbModule,
+    RouterModule.forChild(NegocioRoutes)
   ]
 })
 export class NegocioModule { }
