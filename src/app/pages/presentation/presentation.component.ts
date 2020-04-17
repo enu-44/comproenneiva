23c9da96bb0861e7387dfb41f1d297ff1dffd0d6
julian camilo bonilla leyva <julian.bonilla@interedes.com.co>
2020-04-17 18:02:00 -0500

    Ajustes markers mapa

--- src/app/pages/presentation/presentation.component.ts
+++ src/app/pages/presentation/presentation.component.ts
@@ -16,8 +16,8 @@ export class PresentationComponent implements OnInit {
   zoom: number = 17;
   
   // initial center position for the map
-  lat: number = 51.673858;
-  lng: number = 7.815982;
+  lat: number = 2.934336;
+  lng: number = -75.280828;
 
   clickedMarker(label: string, index: number) {
     console.log(`clicked the marker: ${label || index}`)
@@ -37,22 +37,53 @@ export class PresentationComponent implements OnInit {
   
   markers: marker[] = [
 	  {
-		  lat: 51.673858,
-		  lng: 7.815982,
-		  label: 'A',
-		  draggable: true
+		  lat: 2.934336,
+		  lng: -75.280828,
+      draggable: true,
+      icon: '/assets/img/banco.png',
+      nombre: 'Bancolombia'
+    },
+    {
+		  lat: 2.932567, 
+		  lng: -75.280075,
+      draggable: true,
+      icon: '/assets/img/farma.png',
+      nombre: 'Drogas la economía'
 	  },
 	  {
-		  lat: 51.373858,
-		  lng: 7.215982,
-		  label: 'B',
-		  draggable: false
+		  lat: 2.935552, 
+		  lng: -75.283785,
+      draggable: false,
+      icon: '/assets/img/comida.png',
+      nombre: 'Restaurante La avenida'
 	  },
 	  {
-		  lat: 51.723858,
-		  lng: 7.895982,
-		  label: 'C',
-		  draggable: true
+		  lat: 2.933423, 
+		  lng: -75.277549,
+      draggable: true,
+      icon: '/assets/img/super.png',
+      nombre: 'Supertiendas Olímpica'
+    },
+    {
+		  lat: 2.933673, 
+		  lng: -75.282025,
+      draggable: true,
+      icon: '/assets/img/notaria.png',
+      nombre: 'Notaría Segunda'
+    },
+    {
+		  lat: 2.935302, 
+		  lng: -75.281236,
+      draggable: true,
+      icon: '/assets/img/tienda.png',
+      nombre: 'Tienda la esquina'
+    },
+    {
+		  lat: 2.936862, 
+		  lng: -75.281661,
+      draggable: true,
+      icon: '/assets/img/tienda.png',
+      nombre: 'Tienda doña lola'
 	  }
   ]
 }
@@ -61,6 +92,7 @@ export class PresentationComponent implements OnInit {
 interface marker {
 	lat: number;
 	lng: number;
-	label?: string;
-	draggable: boolean;
+  draggable: boolean;
+  icon: string;
+  nombre: string;
 }
